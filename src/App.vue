ac4cc0d956db0e78dea0d60e1e5369556a945672
anirban12345 <you@example.com>
2022-03-02 16:34:17 +0530

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHome />
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHome from './components/AppHome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHome
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
